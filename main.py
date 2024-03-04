45eb162cad125d003f4ca6e8b502f806f64807b3
Tosharpi <160677912+Tosharpi@users.noreply.github.com>
2024-03-04 09:04:41 -0500

    filtros

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from modules.getClients import searchName 
+import modules.getClients as cliente 
 
-searchName()
+from tabulate import tabulate
+
+
+print(tabulate(cliente.getAllClientPaisRegionCiudad("Australia", "Sur"), tablefmt="grid"))
+
+#coincidir australia y la region (pero si es none tambien mostrarla)
--- modules/getClients.py
+++ modules/getClients.py
@@ -1,4 +1,39 @@
-from storage.cliente import clientes
+import storage.clientes as cli 
 
-def searchName():
-    print(clientes)
+def getAllClientName():
+    clienteName =list()
+    for i, val in enumerate(cli.clientes):
+        codigoName=dict({
+            "codigo_cliente":{val.get('codigo_cliente')},
+            "nombre_cliente":{val.get('nombre_cliente')}
+        })
+        clienteName.append(codigoName)
+    return clienteName
+
+def getOneClientCodigo(codigo):
+    for val in cli.clientes:
+        if(val.get('codigo_cliente') == codigo):
+           return {
+            "codigo_cliente":{val.get('codigo_cliente')},
+            "nombre_cliente":{val.get('nombre_cliente')}
+           }
+        
+def getAllClientCreditoCiudad(limiteCredit, ciudad):
+    clienteCredito =list()
+    for val in cli.clientes:
+        if(val.get('limite_credito') >= limiteCredit and val.get('ciudad') == ciudad):
+            clienteCredito.append(val)
+        
+    return clienteCredito
+
+def getAllClientPaisRegionCiudad(pais, region=None, ciudad=None):
+    clientZone = list()
+    for val in cli.clientes:
+        if (val.get('pais') == pais):
+
+            if (region != None and val.get('region')):
+                clientZone.append(val)
+            elif (val.get('region') == None):
+                clientZone.append(val)
+
+    return clientZone
