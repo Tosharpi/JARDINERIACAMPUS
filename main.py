e49ee7045afea842aa22bc1f017a28658d03fd72
Tosharpi <160677912+Tosharpi@users.noreply.github.com>
2024-03-04 09:06:48 -0500

    filtros

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ from tabulate import tabulate
 print(tabulate(cliente.getAllClientPaisRegionCiudad("Australia", "Sur"), tablefmt="grid"))
 
 #coincidir australia y la region (pero si es none tambien mostrarla)
+#sacar filtros, usuarios sin informacion, usuarios que tengan la misma region
+#
