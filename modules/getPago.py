f238975414ca2d56ff5b232b653a6a7fd8d4932a
Tosharpi <160677912+Tosharpi@users.noreply.github.com>
2024-03-06 14:04:51 -0500

    punto 8 completado

--- modules/getPago.py
+++ modules/getPago.py
@@ -1,18 +1,19 @@
 import storage.pago as pag
 
 def getCodClientesPago2008():
-    getCodClientesPago2008 = []
+    codigosclientespago2008 = []
     for val in pag.pago:
-        año=val.get("fecha_pago")
+        año = val.get("fecha_pago")
 
-        if año[0:4]== "2008":
-            getCodClientesPago2008.append(
+        if año.startswith("2008"):  
+            codigosclientespago2008.append(
                 {
-                    "fecha_pago" : {val.get("fecha_pago")},
-                    "codigo_cliente" : {val.get("codigo_cliente")}
+                    "fecha_pago": val.get("fecha_pago"),
+                    "codigo_cliente": val.get("codigo_cliente")
                 }
             )
-            getCodClientesPago2008=set(getCodClientesPago2008)
-            getCodClientesPago2008=list(getCodClientesPago2008)
 
-    return getCodClientesPago2008
+    codigosclientespago2008 = list(set(tuple(item.items()) for item in codigosclientespago2008))
+    codigosclientespago2008 = [{k: v for k, v in item} for item in codigosclientespago2008]
+
+    return codigosclientespago2008
